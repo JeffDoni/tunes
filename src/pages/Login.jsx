346e3a49fec3b1c77a83864c4bee3b6ed12ddc40
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-12-13 19:49:44 -0300

    Requisito2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  state = {
+    loginname: '',
+    disableButton: true,
+    loading: false,
+  };
+
+  validateInfo = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => {
+      const { loginname } = this.state;
+      const size = 3;
+      const limit = loginname.length < size;
+
+      this.setState({
+        disableButton: limit,
+      });
+    });
+  };
+
+  handleClick = () => {
+    const { history } = this.props;
+    this.setState(
+      { loading: true },
+      async () => {
+        const { loginname } = this.state;
+        await createUser({ name: loginname });
+        return history.push('/search');
+      },
+    );
+  };
+
   render() {
+    const { loginname, disableButton, loading } = this.state;
+    if (loading) return <Carregando />;
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ loginname }
+            onChange={ this.validateInfo }
+            name="loginname"
+          />
+          <button
+            data-testid="login-submit-button"
+            disabled={ disableButton }
+            onClick={ this.handleClick }
+            type="button"
+          >
+            Entrar
+
+          </button>
+        </form>
+      </div>
     );
   }
 }
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
