10b63689427495c2872c0863572bd2f1e0fde22d
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-12-14 11:50:28 -0300

    requisito6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPIs from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
   state = {
     searchText: '',
     disableButtonSearch: true,
+    album: [],
+    failed: true,
+    loading: false,
+    search: '',
   };
 
   validateSearch = ({ target: { name, value } }) => {
@@ -19,8 +26,33 @@ class Search extends React.Component {
     });
   };
 
+  handleClickSearch = () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { searchText } = this.state;
+        const request = await searchAlbumsAPIs(searchText);
+        this.setState({
+          album: request,
+          searchText: '',
+          loading: false,
+          search: searchText,
+        }, () => {
+          if (request.length === 0) {
+            this.setState({ failed: false });
+          }
+        });
+      },
+
+    );
+  };
+
   render() {
-    const { searchText, disableButtonSearch } = this.state;
+    const { searchText,
+      disableButtonSearch,
+      album,
+      search, loading, failed } = this.state;
+    console.log(album);
     return (
       <div data-testid="page-search">
         <Header />
@@ -36,11 +68,34 @@ class Search extends React.Component {
             data-testid="search-artist-button"
             type="button"
             disabled={ disableButtonSearch }
+            onClick={ this.handleClickSearch }
           >
             Pesquisar
 
           </button>
         </form>
+        <div>
+
+          <span>{`Resultado de álbuns de: ${search}`}</span>
+
+          { loading ? (<Carregando />)
+            : (album.map((element) => (
+              <div key={ element.artistId }>
+                <div>
+                  <p>{element.artistName}</p>
+                  <p>{element.collectionName}</p>
+                  <img src={ element.artworkUrl100 } alt={ element.collectionName } />
+                </div>
+                <Link
+                  to={ `/album/${element.collectionId}` }
+                  data-testid={ `link-to-album-${element.collectionId}` }
+                >
+                  Ir para o album
+                </Link>
+              </div>
+            )))}
+          {failed ? null : (<p>Nenhum álbum foi encontrado</p>)}
+        </div>
       </div>
     );
   }
