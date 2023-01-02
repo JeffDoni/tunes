27e694941d44005a253201a0ed7b0b152c340da8
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-01-02 15:20:53 -0300

    correção

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,71 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { BiSearchAlt } from "react-icons/bi";
-import { AiOutlineStar } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { getUser } from "../services/userAPI";
-import Carregando from "../pages/Carregando";
-import "../styles/Header.css";
-import img from "../image/logo.png";
-import imgProfile from "../image/profile.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiSearchAlt } from 'react-icons/bi';
+import { AiOutlineStar } from 'react-icons/ai';
+import { CgProfile } from 'react-icons/cg';
+import { getUser } from '../services/userAPI';
+import Carregando from '../pages/Carregando';
+import '../styles/Header.css';
+import img from '../image/logo.png';
+import imgProfile from '../image/profile.png';
 
 class Header extends React.Component {
-  state = {
-    loading: true,
-    id: {},
-  };
+    state = {
+        loading: true,
+        id: {},
+    };
 
-  componentDidMount() {
-    this.setState({ loading: true }, async () => {
-      const request = await getUser();
-      this.setState({
-        loading: false,
-        id: request,
-      });
-    });
-  }
+    componentDidMount() {
+        this.setState({ loading: true }, async () => {
+            const request = await getUser();
+            this.setState({
+                loading: false,
+                id: request,
+            });
+        });
+    }
 
-  render() {
-    const { loading, id } = this.state;
-    return (
-      <header data-testid="header-component" className="header">
-        <img src={img} alt="logo" />
-        <nav>
-          <Link to="/search" data-testid="link-to-search" className="link">
-            <BiSearchAlt />
+    render() {
+        const { loading, id } = this.state;
+        return (
+            <header data-testid="header-component" className="header">
+                <img src={img} alt="logo" />
+                <nav>
+                    <Link to="/search" data-testid="link-to-search" className="link">
+                        <BiSearchAlt />
             Busca
-          </Link>
+                    </Link>
 
-          <Link
-            to="/favorites"
-            data-testid="link-to-favorites"
-            className="link"
-          >
-            <AiOutlineStar />
+                    <Link
+                        to="/favorites"
+                        data-testid="link-to-favorites"
+                        className="link"
+                    >
+                        <AiOutlineStar />
             Favoritas
-          </Link>
-          <Link to="/profile" data-testid="link-to-profile" className="link">
-            <CgProfile />
+                    </Link>
+                    <Link to="/profile" data-testid="link-to-profile" className="link">
+                        <CgProfile />
             Profile
-          </Link>
-        </nav>
-        <div className="info-user">
-          <div>
-            <img
-              src={id.image || imgProfile}
-              alt={id.name}
-              className="image-profile"
-            />
-            <p data-testid="header-user-name">{id.name}</p>
-          </div>
-          <div>
-            <Link to="/">Sair</Link>
-          </div>
-          <span>{loading && <Carregando />}</span>
-        </div>
-      </header>
-    );
-  }
+                    </Link>
+                </nav>
+                <div className="info-user">
+                    <div>
+                        <img
+                            src={id.image || imgProfile}
+                            alt={id.name}
+                            className="image-profile"
+                        />
+                        <p data-testid="header-user-name">{id.name}</p>
+                    </div>
+                    <div>
+                        <Link to="/">Sair</Link>
+                    </div>
+                    <span>{loading && <Carregando />}</span>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,70 +6,70 @@ import '../styles/Login.css';
 import img from '../image/logo.png';
 
 class Login extends React.Component {
-  state = {
-    loginname: '',
-    disableButton: true,
-    loading: false,
-  };
+    state = {
+        loginname: '',
+        disableButton: true,
+        loading: false,
+    };
 
-  validateInfo = ({ target: { name, value } }) => {
-    this.setState({ [name]: value }, () => {
-      const { loginname } = this.state;
-      const size = 3;
-      const limit = loginname.length < size;
+    validateInfo = ({ target: { name, value } }) => {
+        this.setState({ [name]: value }, () => {
+            const { loginname } = this.state;
+            const size = 3;
+            const limit = loginname.length < size;
 
-      this.setState({
-        disableButton: limit,
-      });
-    });
-  };
+            this.setState({
+                disableButton: limit,
+            });
+        });
+    };
 
-  handleClick = () => {
-    const { history } = this.props;
-    this.setState(
-      { loading: true },
-      async () => {
-        const { loginname } = this.state;
-        await createUser({ name: loginname });
-        return history.push('/search');
-      },
-    );
-  };
+    handleClick = () => {
+        const { history } = this.props;
+        this.setState(
+            { loading: true },
+            async () => {
+                const { loginname } = this.state;
+                await createUser({ name: loginname });
+                return history.push('/search');
+            },
+        );
+    };
 
-  render() {
-    const { loginname, disableButton, loading } = this.state;
-    if (loading) return <Carregando />;
+    render() {
+        const { loginname, disableButton, loading } = this.state;
+        if (loading) return <Carregando />;
 
-    return (
-      <div data-testid="page-login" className="login">
-        <img src={ img } alt="logo" />
-        <form>
-          <input
-            type="text"
-            data-testid="login-name-input"
-            value={ loginname }
-            onChange={ this.validateInfo }
-            name="loginname"
-            placeholder='Digite seu nome'
-          />
-          <button
-            data-testid="login-submit-button"
-            disabled={ disableButton }
-            onClick={ this.handleClick }
-            type="button"
-          >
+        return (
+            <div data-testid="page-login" className="login">
+                <img src={ img } alt="logo" />
+                <form>
+                    <input
+                        type="text"
+                        data-testid="login-name-input"
+                        value={ loginname }
+                        onChange={ this.validateInfo }
+                        name="loginname"
+                        placeholder='Digite seu nome'
+                    />
+                    <button
+                        data-testid="login-submit-button"
+                        disabled={ disableButton }
+                        onClick={ this.handleClick }
+                        type="button"
+                    >
             Entrar
 
-          </button>
-        </form>
-      </div>
-    );
-  }
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
 Login.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
+    history: PropTypes.shape({
+        push: PropTypes.func,
+    }).isRequired,
 };
 
 export default Login;
